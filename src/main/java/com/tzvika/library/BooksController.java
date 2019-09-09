41c9e98c3595a1c92f1ce149e87b62936139f263
Tzvika Ofek <tofek@nexway.com>
2019-09-09 14:16:12 +0200

    added create book method

--- src/main/java/com/tzvika/library/BooksController.java
+++ src/main/java/com/tzvika/library/BooksController.java
@@ -3,9 +3,7 @@ package com.tzvika.library;
 import com.tzvika.library.models.Book;
 import com.tzvika.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,10 @@ public class BooksController {
     @GetMapping("/")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public void createBook(@RequestBody Book book) {
+        bookRepository.save(book);
     }
 }
